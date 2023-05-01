9028292e5224136294c3dd1138f339070b493f6f
sarvjeet.bhardwaj <sjitbh121993@gmail.com>
2023-05-01 20:24:04 +0530

    Added Image upload feature

--- Social_Media_App_HTML_CSS_Django/social_media_app/core/views.py
+++ Social_Media_App_HTML_CSS_Django/social_media_app/core/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username= request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile':user_profile})
+
 
 def signup(request):
     if request.method == 'POST':
@@ -31,20 +35,65 @@ def signup(request):
                 user.save()
 
                 ## Login in user and direct to settings page
-
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                
                 ## create a profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user = user_model.id)
                 new_profile.save()
                 messages.info(request, 'New user created !!')
-                return redirect('signup')
+                return redirect('settings')
 
         else:
             messages.info(request, 'Password donot match')
             return redirect('signup')
     else:
         return render(request, 'signup.html')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') !=None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
     
+    return render(request, 'setting.html',{'user_profile':user_profile})
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        return redirect('/')
+    else:
+        return redirect('/')
+
 
 def signin(request):
     if request.method == 'POST':
@@ -58,10 +107,13 @@ def signin(request):
         else:
             messages.info(request, 'Credentials Invalid')
             return redirect('signin')
-
     else:
         return render(request, 'signin.html')
 
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
 
     
 
