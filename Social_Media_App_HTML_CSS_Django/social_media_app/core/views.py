15b725c3b6a9452b79de98c44ff4abb7bd564c04
sarvjeet.bhardwaj <sjitbh121993@gmail.com>
2023-05-02 13:11:38 +0530

    Added Like feature and Feed Posts feature

--- Social_Media_App_HTML_CSS_Django/social_media_app/core/views.py
+++ Social_Media_App_HTML_CSS_Django/social_media_app/core/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile, Post
+from .models import Profile, Post , LikePost
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='signin')
 def index(request):
     user_object = User.objects.get(username= request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    return render(request, 'index.html', {'user_profile':user_profile})
+    posts = Post.objects.all()
+
+    return render(request, 'index.html', {'user_profile':user_profile,'posts':posts})
 
 
 def signup(request):
@@ -94,6 +96,30 @@ def upload(request):
     else:
         return redirect('/')
 
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes + 1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes - 1
+        post.save()
+        return redirect('/')
+
+
+def profile(request):
+    return render(request, 'profile.html')
+
 
 def signin(request):
     if request.method == 'POST':
@@ -114,6 +140,7 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
 
     
 
