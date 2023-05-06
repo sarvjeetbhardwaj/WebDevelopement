0be24364af85d793403c08ced99b2337dd134c9d
sarvjeet.bhardwaj <sjitbh121993@gmail.com>
2023-05-06 14:52:41 +0530

    Final Update

--- Social_Media_App_HTML_CSS_Django/social_media_app/core/views.py
+++ Social_Media_App_HTML_CSS_Django/social_media_app/core/views.py
@@ -4,14 +4,56 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Profile, Post , LikePost, FollowerCount
 from django.contrib.auth.decorators import login_required
+from itertools import chain
+import random
 
 @login_required(login_url='signin')
 def index(request):
     user_object = User.objects.get(username= request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    posts = Post.objects.all()
+    
+    user_following_list = []
+    feed = []
 
-    return render(request, 'index.html', {'user_profile':user_profile,'posts':posts})
+    user_following = FollowerCount.objects.filter(follower = request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+
+    for username in user_following_list:
+        feed_lists = Post.objects.filter(user=username)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
+
+    #user_suggestion
+    all_users = User.objects.all()
+    user_following_all = []
+
+    for user in user_following:
+        user_list = User.objects.get(username = user.user)
+        user_following_all.append(user_list)
+
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user = ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile':user_profile,'posts':feed_list, 'suggestions_username_profile_list':suggestions_username_profile_list[:4]})
 
 
 def signup(request):
@@ -183,6 +225,25 @@ def follow(request):
     else:
         return redirect('/')
     
+@login_required(login_url='signin')
+def search(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
 
+    if request.method == 'POST':
+        username =  request.POST['username']
+        username_object = User.objects.filter(username__icontains = username)
 
-    
+        username_profile = []
+        username_profile_list = []
+
+        for users in username_object:
+            username_profile.append(users.id)
+        
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+
+        username_profile_list = list(chain(*username_profile_list))
+
+    return render(request, 'search.html',{'user_profile':user_profile,'username_profile_list':username_profile_list})
